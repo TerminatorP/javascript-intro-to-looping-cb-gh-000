ddc59b703d09028c8948248572036e5bc3eb7b3b
Prabesh Subedi <subediprabesh1@gmail.com>
2018-04-09 05:58:19 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -6,3 +6,19 @@ function forLoop(arr) {
     arr.push(`I am ${i} strange loops.`);
   }
 }
+
+function whileLoop(arr) {
+  while (arr >= 0) {
+    console.log(arr--);
+  }
+  return 'done';
+}
+
+function maybeTrue() {
+  return Math.random() >= 0.5;
+}
+function doWhileLoop(arr) {
+  do {
+    arr.pop();
+  } while (maybeTrue() && arr.length > 0);
+}
