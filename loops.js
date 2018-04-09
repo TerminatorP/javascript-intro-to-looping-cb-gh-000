2b9a1a81cb458b143721878ed6d1fb142e99b5d7
Prabesh Subedi <subediprabesh1@gmail.com>
2018-04-09 06:04:06 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,9 +1,6 @@
 function forLoop(arr) {
   for (let i = 0; i < 25; i++) {
-    if (i === 1) {
-      arr.push("I am 1 strange loop.");
-    }
-    arr.push(`"I am ${i} strange loops."`);
+    arr.push(`"I am ${i} strange loop${i === 0 ? '' : 's'}."`);
   }
 }
 
